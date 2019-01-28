6a49bd52e824469100e7de224e019b8454b12407
Lukas <lukas.pfahler@tu-dortmund.de>
2019-01-28 16:39:16 +0100

    sample_weights

--- python/weightedFastText/FastTextEstimator.py
+++ python/weightedFastText/FastTextEstimator.py
@@ -2,15 +2,16 @@ from sklearn.base import ClassifierMixin,BaseEstimator
 from weightedFastText import train_supervised
 import numpy as np	
 class FastTextEstimator(ClassifierMixin,BaseEstimator):
-	def __init__(self,wordNgrams=1,minn=0,maxn=0,epoch=10,dim=100,verbose=0):
+	def __init__(self,wordNgrams=1,minn=0,maxn=0,epoch=10,dim=100,verbose=0,pretrained=""):
 		self.wordNgrams = wordNgrams
 		self.minn = minn
 		self.maxn = maxn
 		self.epoch = epoch
 		self.dim = dim
 		self.verbose = verbose
+		self.pretrainedVectors = pretrained
 		super(ClassifierMixin, self).__init__()
-	def fit(self,X,y,weights = None,progressbar=None):
+	def fit(self,X,y,sample_weights = None,progressbar=None):
 		import tempfile,os
 
 		# Write Weights to Binary File for FastText
@@ -21,7 +22,7 @@ class FastTextEstimator(ClassifierMixin,BaseEstimator):
 		if weights is None:
 			s = struct.pack('f'*len(X), *(len(X)*[1.0]))
 		else:
-			s = struct.pack('f'*len(X), *[len(x) * w for w in weights])
+			s = struct.pack('f'*len(X), *[len(X) * w for w in weights])
 		handleWeights.write(s)
 		handleWeights.close()
 
@@ -42,8 +43,9 @@ class FastTextEstimator(ClassifierMixin,BaseEstimator):
 			wordNgrams=self.wordNgrams,
 			minn = self.minn,
 			maxn = self.maxn,
-			epoch=self.epoch,
-			verbose=self.verbose
+			epoch = self.epoch,
+			verbose=self.verbose,
+			pretrainedVectors = self.pretrainedVectors
 		)
 		self.num_labels = len(self._model.get_labels())
 		os.remove(handleTrain.name)
