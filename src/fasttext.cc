999ed49e2164d04500d29a2149e0cad3c5548373
Lukas Pfahler <lukas.pfahler@udo.edu>
2019-01-30 15:57:39 +0100

    renamed some stuff and increased version number

--- src/fasttext.cc
+++ src/fasttext.cc
@@ -761,7 +761,7 @@ void FastText::train(const Args& args) {
 	// weights_->load(0);
 	if (args_->pretrainedVectors.size() != 0) {
 		loadVectors(args_->pretrainedVectors);
-		std::cerr << "Number of words:  " << dict_->nwords() << std::endl;
+		// std::cerr << "Number of words:  " << dict_->nwords() << std::endl;
 	} else {
 		input_ =
 				std::make_shared<Matrix>(dict_->nwords() + args_->bucket, args_->dim);
